b77bc7247aad678b6aa4a3049398e413daedd0c4
E-Rx <elise.rochaix@gmail.com>
2025-06-09 14:01:51 +0200

    Entity Map

---
+++ UsersApp.Domain/Entity/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp.Domain.Entity
+{
+    internal class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string ISBN { get; set; } = null!;
+
+        public bool IsAvailable { get; set; } = true;
+
+    }
+}
