6a453c97405452b8419f39ed187f2ab1612f5216
1256ID <112385041+1256ID@users.noreply.github.com>
2025-06-10 16:51:46 +0200

    Adding controller methods.

--- UsersApp.Web/Controllers/UserController.cs
+++ UsersApp.Web/Controllers/UserController.cs
@@ -1,20 +1,42 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using UsersApp.Application.Services.Users;
+using UsersApp.Application.Interfaces.Users;
+using UsersApp.Web.Views.User;
 
 namespace UsersApp.Web.Controllers;
 
 [Authorize]
-public class UserController(UserService userService) : Controller
+public class UserController(IUserService userService) : Controller
 {
     [HttpGet("")]
     [AllowAnonymous]
     public async Task<IActionResult> Index()
+    {       
+        return RedirectToAction(nameof(Login));
+    }
+
+    [HttpGet("/members")]   
+    public async Task<IActionResult> Member()
     {
-        // var model = userService.
         return View();
     }
 
+    //////////////////////////////////////////////////////
+
+    [HttpGet("/login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost("/login")]
+    public async Task<IActionResult> Login(LoginVM loginVM)
+    {
+
+
+        return RedirectToAction(nameof(Member));
+    }
+
+    //////////////////////////////////////////////////////
 
     [HttpGet("/register")]
     public IActionResult Register()
@@ -24,7 +46,7 @@ public class UserController(UserService userService) : Controller
     [HttpPost("/register")]
     public async Task<IActionResult> Register(RegisterVM registerVM)
     {
-        return RedirectToAction(nameof(Index));
+        return RedirectToAction(nameof(Member));
     }
 
     
