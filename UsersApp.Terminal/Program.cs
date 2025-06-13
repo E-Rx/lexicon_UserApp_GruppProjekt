8ad28693c23c51895cd276b881447b929729d575
1256ID <112385041+1256ID@users.noreply.github.com>
2025-06-13 16:12:27 +0200

    Adding Terminal project.

---
+++ UsersApp.Terminal/Program.cs
@@ -0,0 +1,26 @@
+﻿using UsersApp.Application.Interfaces;
+using UsersApp.Application.Interfaces.Users;
+using UsersApp.Application.Services.Users;
+namespace UsersApp.Terminal;
+
+public class ConsoleApp
+{
+
+    
+
+    public static void Main()
+    {
+        
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
